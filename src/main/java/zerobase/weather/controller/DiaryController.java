c94eaf87c614e3569f1ce83c106339a33a50c3d8
eunsun <eunsunjj9501@gmail.com>
2024-03-29 01:20:17 +0900

    Insert data into Database

--- src/main/java/zerobase/weather/controller/DiaryController.java
+++ src/main/java/zerobase/weather/controller/DiaryController.java
@@ -1,10 +1,7 @@
 package zerobase.weather.controller;
 
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import zerobase.weather.service.DiaryService;
 
 import java.time.LocalDate;
