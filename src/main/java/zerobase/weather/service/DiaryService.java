a57abee79b02160d94e942aee23685db4c4f2677
eunsun <eunsunjj9501@gmail.com>
2024-03-28 17:18:06 +0900

    Parsing Open API

--- src/main/java/zerobase/weather/service/DiaryService.java
+++ src/main/java/zerobase/weather/service/DiaryService.java
@@ -1,5 +1,8 @@
 package zerobase.weather.service;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +11,8 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class DiaryService {
@@ -19,6 +24,7 @@ public class DiaryService {
         String weatherData = getWeatherString();
 
         // 2. 받아온 날씨 데이터 (json) 파싱하기
+        Map<String, Object> parseWeather = parseWeather(weatherData);
 
         // 3. 파싱된 데이터 + 일기 값 우리 DB에 넣기
     }
@@ -48,5 +54,26 @@ public class DiaryService {
         }catch (Exception e) {
             return "failed to get response";
         }
+    }
+
+    private Map<String, Object> parseWeather(String jsonString){
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject;
+
+        try{
+            jsonObject = (JSONObject) jsonParser.parse(jsonString);
+        }catch (ParseException e){
+            throw new RuntimeException(e); // 예외가 났다!!
+        }
+
+        Map<String, Object> resultMap = new HashMap<>();
+
+        JSONObject mainData = (JSONObject) jsonObject.get("main");
+        resultMap.put("temp", mainData.get("temp"));
+        JSONObject weatherData = (JSONObject) jsonObject.get("weather");
+        resultMap.put("main", weatherData.get("main"));
+        resultMap.put("icon", weatherData.get("icon"));
+
+        return resultMap;
     }
 }
